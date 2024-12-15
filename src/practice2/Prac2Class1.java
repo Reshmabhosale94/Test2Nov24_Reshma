1bd565bd801be84f5e44ed001e2f418c42c070f3
Reshmabhosale <reshmabhosale4141@.com>
2024-12-16 00:11:47 +0530

    Branch A changes

--- src/practice2/Prac2Class1.java
+++ src/practice2/Prac2Class1.java
@@ -3,7 +3,6 @@ package practice2;
 public class Prac2Class1 {
 	
 	public static void main(String[] args) {		
-		System.out.println("Hello_class1, branch A changes");
-		System.out.println("Branch B changes");
-	}
+		System.out.println("Hello_class1, branch A changes for second scenario");
+		}
 }
