276da75d169831f3f4110e90da9ada4959c704be
Reshmabhosale <reshmabhosale4141@.com>
2024-12-16 00:18:13 +0530

    Branch B changes

--- src/practice2/Prac2Class1.java
+++ src/practice2/Prac2Class1.java
@@ -4,6 +4,6 @@ public class Prac2Class1 {
 	
 	public static void main(String[] args) {		
 		System.out.println("Hello_class1, branch A changes");
-		System.out.println("Branch B changes");
+		System.out.println("Branch B changes, Conflict second scenario");
 	}
 }
