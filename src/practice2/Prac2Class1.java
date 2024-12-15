5a339412090dc003c429a2c43dc277da5d591aeb
Reshmabhosale <reshmabhosale4141@.com>
2024-12-15 23:51:57 +0530

    Conflict resolved with branch code

--- src/practice2/Prac2Class1.java
+++ src/practice2/Prac2Class1.java
@@ -2,7 +2,8 @@ package practice2;
 
 public class Prac2Class1 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		System.out.println("Hello_class1, branch A changes");
+		System.out.println("Branch B changes");
 	}
 }
