55771ebdbc7e76e60c95685a0227de11bb5d2f27
Reshmabhosale <reshmabhosale4141@.com>
2024-12-29 11:53:38 +0530

    OldBranch Commit

---
+++ src/practice2/Example1.java
@@ -0,0 +1,25 @@
+package practice2;
+
+public class Example1 {
+int balance=1100;
+	
+	void creditAmt(){
+		balance=balance + 300;
+	}
+	
+	void debitAmt(){
+		balance=balance - 500;
+		}
+		
+	void display(){
+		System.out.println("Current balance is " + balance);
+	}
+
+	public static void main (String[] args){
+		Example1 b=new Example1();
+		b.creditAmt();
+		b.debitAmt();
+		b.creditAmt();
+		b.display();
+	}
+}
